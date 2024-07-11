dea27569b86362f73035bc905f6e0d21c3652c7f
Stephanie <stephaniemsegura09@gmail.com>
2024-07-11 17:46:47 -0500

    useEffect fetch setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch('http://localhost:3001/entries');
+      const jsonData = await response.json();
+      setData(jsonData);
+      console.log(jsonData)
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello World</h1>
+      {data.map((item) => (
+        <p key={item.entry_id}>{item.title}</p>
+      ))}
     </div>
   );
 }
