92bad2bb0e68c42e4f2aabd4daad568a36c2cf15
Stephanie <stephaniemsegura09@gmail.com>
2024-07-11 18:48:54 -0500

    imported bootstrap

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './components/HomePage';
 import AboutPage from './components/AboutPage';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   // const [data, setData] = useState([]);
