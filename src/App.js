91211b82a91a821848d3048b1809b66b6cf99b78
Stephanie <stephaniemsegura09@gmail.com>
2024-07-11 18:42:19 -0500

    Integrated react router dom and basic routes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './components/HomePage';
+import AboutPage from './components/AboutPage';
 
 function App() {
-  const [data, setData] = useState([]);
+  // const [data, setData] = useState([]);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const response = await fetch('http://localhost:3001/entries');
-      const jsonData = await response.json();
-      setData(jsonData);
-      console.log(jsonData)
-    };
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     const response = await fetch('http://localhost:3001/entries');
+  //     const jsonData = await response.json();
+  //     setData(jsonData);
+  //     console.log(jsonData)
+  //   };
 
-    fetchData();
-  }, []);
+  //   fetchData();
+  // }, []);
 
   return (
-    <div>
-      <h1>Hello World</h1>
-      {data.map((item) => (
-        <p key={item.entry_id}>{item.title}</p>
-      ))}
-    </div>
+    <Router>
+      <div>
+        {/* Navigation Links */}
+        <nav>
+          <ul>
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="/about">About</a>
+            </li>
+            <li>
+              <a href="/contact">Contact</a>
+            </li>
+          </ul>
+        </nav>
+
+        {/* Route Definitions */}
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
+          {/* <Route path="/contact" element={<ContactPage />} /> */}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/AboutPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function AboutPage() {
+  
+  return (
+    <div>
+      <h1>Hello AbtPage</h1>
+    </div>
+  );
+}
+
+export default AboutPage;
